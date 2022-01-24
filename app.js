5b471072e4934825a2dbecd03e35ad3472c05c20
Dylan Jacob <business.dmjacob@gmail.com>
2022-01-24 10:46:03 -0500

    added a few if statements to add a zero in front of the time if it was in single digits. This prevents the border radius from shrinking and also allows for continuity throughout the timer

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const timerDayEl = document.querySelector(".timer__day");
 const timerHourEl = document.querySelector(".timer__hour");
 const timerMinEl = document.querySelector(".timer__min");
 const timerSecEl = document.querySelector(".timer__sec");
-const startDate = new Date();
-const endDate = new Date("March 16, 2022 01:00:00");
+
 
 function getTimeDifference(start, end) {
     let millieseconds = Math.floor(end - start);
@@ -11,11 +10,11 @@ function getTimeDifference(start, end) {
     let minutes = Math.floor(seconds / 60);
     let hours = Math.floor(minutes / 60);
     let days = Math.floor(hours / 24);
-    
+
     hours = hours - (days * 24);
     minutes = minutes - (days * 24 * 60) - (hours * 60);
     seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
-    
+
     return {
         rDays: days,
         rHours: hours,
@@ -25,5 +24,32 @@ function getTimeDifference(start, end) {
 
 }
 
-console.log(getTimeDifference(startDate, endDate));
+let timer = setInterval(function () {
+    const startDate = new Date();
+    const endDate = new Date("Januray 29, 2022 11:00:00");
 
+    let timeDifferenceObj = getTimeDifference(startDate, endDate);
+    timerDayEl.textContent = timeDifferenceObj.rDays;
+    timerHourEl.textContent = timeDifferenceObj.rHours;
+    timerMinEl.textContent = timeDifferenceObj.rMinutes;
+    timerSecEl.textContent = timeDifferenceObj.rSeconds;
+
+
+    if (timeDifferenceObj.rDays.toString().length < 2) {
+        timerDayEl.textContent = "0" + timeDifferenceObj.rDays
+    } 
+    if (timeDifferenceObj.rHours.toString().length < 2) {
+        timerHourEl.textContent = "0" + timeDifferenceObj.rHours
+    }
+    if (timeDifferenceObj.rMinutes.toString().length < 2) {
+        timerMinEl.textContent = "0" + timeDifferenceObj.rMinutes
+    }
+    if (timeDifferenceObj.rSeconds.toString().length < 2) {
+        timerSecEl.textContent = "0" + timeDifferenceObj.rSeconds
+    }
+
+    if (startDate === endDate) {
+        console.log("end has been reached")
+    }
+
+}, 1000);
